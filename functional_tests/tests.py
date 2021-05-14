5fa767b3c8801f2cba5f6fc1cd781b2566823ce4
longwei zhang <longweizhang@long.local>
2021-05-14 13:22:46 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,39 +1,41 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
 import time
-#从selenium引入webdriver
 
-class NewVisitorTest(unittest.TestCase):  #(1)
-    
-    def setUp(self):    #(3)
+
+# 从selenium引入webdriver
+
+class NewVisitorTest(LiveServerTestCase):  # (1)
+
+    def setUp(self):  # (3)
         self.browser = webdriver.Firefox()
-        
-    def tearDown(self): #(3)
+
+    def tearDown(self):  # (3)
         self.browser.quit()
-        
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-        
-    def test_can_start_a_list_and_retrieve_it_later(self): #(2)
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  # (2)
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
-        
+        self.browser.get(self.live_server_url)
+
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
+
         # She is invited to enter a to-do item s traight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-            )
-            
+        )
+
         # She types "Buy peacock feathers" into a text box ( Edith's hobby
         # is tying fly-fishing lures )
         inputbox.send_keys('Buy peacock feathers')
@@ -60,9 +62,10 @@ class NewVisitorTest(unittest.TestCase):  #(1)
         # explanatory text to that effect.
         self.fail('Finish the test!')
 
-
         # She visits that URL - her to-do list is still there .
-if __name__ == '__main__':              #(6)
-    unittest.main(argv=['ignored', '-v'], exit=False)  #(7)
 
-        # Satisfied, she goes back to sleep
+
+# if __name__ == '__main__':  # (6)
+    # unittest.main(argv=['ignored', '-v'], exit=False)  # (7)
+
+    # Satisfied, she goes back to sleep
